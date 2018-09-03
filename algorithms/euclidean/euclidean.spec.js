ff47dee3d026272d95db1d27e180cc82959f2241
Isaac Ramirez <iramirezc@live.com.mx>
2018-09-03 02:04:36 -0500

    move specs as json

--- algorithms/euclidean/euclidean.spec.js
+++ algorithms/euclidean/euclidean.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const euclidean = require('./euclidean');
+const testCases = require('./data/euclidean.test-cases.json');
 const { INTEGER_TYPE_ERROR } = require('../../utils/errors');
 
 describe('Algorithm - Euclidean [Greatest Common Divisor]', function () {
@@ -15,14 +16,6 @@ describe('Algorithm - Euclidean [Greatest Common Divisor]', function () {
   });
 
   describe('Test cases', function () {
-    const testCases = [
-      { input: [3, 0], expected: 3 },
-      { input: [0, 5], expected: 5 },
-      { input: [54, 24], expected: 6 },
-      { input: [180, 48], expected: 12 },
-      { input: [15996751, 3870378], expected: 4057 }
-    ];
-
     let result;
     testCases.forEach(({ input, expected }) => {
       it(`should return: ${expected} for euclidean(${input.join(', ')})`, function () {

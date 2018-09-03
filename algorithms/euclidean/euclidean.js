7eb097c428e39c6b63c48c136f351b90eb617717
Isaac Ramirez <iramirezc@live.com.mx>
2018-09-03 02:43:51 -0500

    remove error handling

--- algorithms/euclidean/euclidean.js
+++ algorithms/euclidean/euclidean.js
@@ -1,16 +1,7 @@
-const { validateIsInteger } = require('../../utils');
-
 function euclidean(p, q) {
-  validateIsInteger('p')(p);
-  validateIsInteger('q')(q);
-
-  function gcd(p, q) {
-    if (q === 0) return p;
-    let r = p % q;
-    return gcd(q, r);
-  }
-
-  return gcd(p, q);
+  if (q === 0) return p;
+  let r = p % q;
+  return euclidean(q, r);
 }
 
 module.exports = euclidean;
--- algorithms/euclidean/euclidean.spec.js
+++ algorithms/euclidean/euclidean.spec.js
@@ -2,19 +2,8 @@ const assert = require('assert');
 
 const euclidean = require('./euclidean');
 const testCases = require('./data/euclidean.test-cases.json');
-const { INTEGER_TYPE_ERROR } = require('../../utils/errors');
 
 describe('Algorithm - Euclidean [Greatest Common Divisor]', function () {
-  describe('Error handling', function () {
-    it('should throw an error when "p" is not an integer', function () {
-      assert.throws(() => euclidean('a', 1), INTEGER_TYPE_ERROR, 'argument "p" should throw an error');
-    });
-
-    it('should throw an error when "q" is not an integer', function () {
-      assert.throws(() => euclidean(1, null), INTEGER_TYPE_ERROR, 'argument "q" should throw an error');
-    });
-  });
-
   describe('Test cases', function () {
     let result;
     testCases.forEach(({ input, expected }) => {

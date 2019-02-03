8cafd223437ec9be5e2913d541be6ad508b06a13
Isaac Ramirez <iramirezc@live.com.mx>
2019-02-02 21:28:24 -0600

    add InsertionSort algorithm

---
+++ chapter/2-sorting/section/1/algorithm/InsertionSort.java
@@ -0,0 +1,62 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Type: Algorithm
+ * Description: InsertionSort Algorithm
+ * Ref: p. 251
+ *
+ * Compilation: javac InsertionSort.java
+ * Execution: java InsertionSort < <input_file>
+ *
+ * java InsertionSort < ~/algs4-data/tiny.txt
+ * A E E L M O P R S T X
+ */
+
+/**
+ * InsertionSort
+ */
+public class InsertionSort {
+  public static void sort(Comparable[] a) {
+    int n = a.length;
+
+    for (int i = 1; i < n; i++) {
+      for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+        exchange(a, j, j - 1);
+      }
+    }
+  }
+
+  private static boolean less(Comparable v, Comparable w) {
+    return v.compareTo(w) < 0;
+  }
+
+  private static void exchange(Comparable[] a, int i, int j) {
+    Comparable t = a[i];
+    a[i] = a[j];
+    a[j] = t;
+  }
+
+  private static void show(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      StdOut.print(a[i] + " ");
+    }
+    StdOut.println();
+  }
+
+  private static boolean isSorted(Comparable[] a) {
+    for (int i = 1; i < a.length; i++) {
+      if (less(a[i], a[i - 1]))
+        return false;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    String[] a = StdIn.readAllStrings();
+    sort(a);
+    assert isSorted(a);
+    show(a);
+    StdOut.printf("isSorted? %s\n", isSorted(a));
+  }
+}

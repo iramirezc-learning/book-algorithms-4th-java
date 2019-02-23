9a8f5c198fb9e10446fc4c3822e0deec1d272aab
Isaac Ramirez <iramirezc@live.com.mx>
2019-02-23 12:16:06 -0600

    add equals method to AleteranteDate

--- chapter/1-fundamentals/section/2/data-type/AlternateDate.java
+++ chapter/1-fundamentals/section/2/data-type/AlternateDate.java
@@ -10,6 +10,12 @@ import edu.princeton.cs.algs4.StdOut;
  *
  * $ java AlternateDate 9 27 1987
  * 9/27/1897
+ * Is 9/27/1987 equals to 9/27/1987? true
+ * Is 9/27/1987 equals to 9/27/1987? true
+ * Is 9/27/1987 equals to null? false
+ * Is 9/27/1987 equals to 9/27/1988? false
+ * Is 9/27/1987 equals to 10/27/1988? false
+ * Is 9/27/1987 equals to 9/26/1988? false
  */
 
 /**
@@ -38,6 +44,23 @@ public class AlternateDate {
     return month() + "/" + day() + "/" + year();
   }
 
+  public boolean equals(Object x) {
+    if (this == x)
+      return true;
+    if (x == null)
+      return false;
+    if (this.getClass() != x.getClass())
+      return false;
+    AlternateDate that = (AlternateDate) x;
+    if (this.year() != that.year())
+      return false;
+    if (this.month() != that.month())
+      return false;
+    if (this.day() != that.day())
+      return false;
+    return true;
+  }
+
   public static void main(String[] args) throws Exception {
     if (args.length != 3) {
       throw new Error("You must provide: <month> <day> <year>");
@@ -50,5 +73,13 @@ public class AlternateDate {
     AlternateDate birthday = new AlternateDate(m, d, y);
 
     StdOut.println(birthday);
+
+    AlternateDate date = new AlternateDate(9, 27, 1987);
+    StdOut.printf("Is %s equals to %s? %b\n", date, date, date.equals(date));
+    StdOut.printf("Is %s equals to %s? %b\n", date, "9/27/1987", date.equals(new AlternateDate(9, 27, 1987)));
+    StdOut.printf("Is %s equals to %s? %b\n", date, null, date.equals(null));
+    StdOut.printf("Is %s equals to %s? %b\n", date, "9/27/1988", date.equals(new AlternateDate(9, 27, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", date, "10/27/1988", date.equals(new AlternateDate(10, 27, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", date, "9/26/1988", date.equals(new AlternateDate(9, 26, 1988)));
   }
 }

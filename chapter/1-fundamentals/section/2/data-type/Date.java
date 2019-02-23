8d830416a4894220a8b83dc2c2a07affc5c3a4b9
Isaac Ramirez <iramirezc@live.com.mx>
2019-02-23 12:15:23 -0600

    add method compares to and equals to Date

--- chapter/1-fundamentals/section/2/data-type/Date.java
+++ chapter/1-fundamentals/section/2/data-type/Date.java
@@ -3,19 +3,28 @@ import edu.princeton.cs.algs4.StdOut;
 /**
  * Type: ADT
  * Description: An abstract data type to encapsulate dates.
- * Ref: p. 91
+ * Ref: p. 91, p. 247
  *
  * Compilation: javac Date.java
  * Execution: java Date <month> <day> <year>
  *
  * $ java Date 9 27 1987
- * 9/27/1897
+ * Your Date is: 9/27/1987
+ * equals()
+ * Is 9/27/1987 equals to 9/27/1987? true
+ * Is 9/27/1987 equals to 9/27/1987? true
+ * Is 9/27/1987 equals to null? false
+ * Is 9/27/1987 equals to 9/27/1988? false
+ * Is 9/27/1987 equals to 10/27/1988? false
+ * Is 9/27/1987 equals to 9/26/1988? false
+ * compareTo()
+ * 9/27/1987 <= 9/27/1987 ? true
  */
 
 /**
  * Date
  */
-public class Date {
+public class Date implements Comparable<Date> {
   private final int month;
   private final int day;
   private final int year;
@@ -42,6 +51,39 @@ public class Date {
     return month() + "/" + day() + "/" + year();
   }
 
+  public boolean equals(Object x) {
+    if (this == x)
+      return true;
+    if (x == null)
+      return false;
+    if (this.getClass() != x.getClass())
+      return false;
+    Date that = (Date) x;
+    if (this.year() != that.year())
+      return false;
+    if (this.month() != that.month())
+      return false;
+    if (this.day() != that.day())
+      return false;
+    return true;
+  }
+
+  public int compareTo(Date that) {
+    if (this.year > that.year)
+      return +1;
+    if (this.year < that.year)
+      return -1;
+    if (this.month > that.month)
+      return +1;
+    if (this.month < that.month)
+      return -1;
+    if (this.day > that.day)
+      return +1;
+    if (this.day < that.day)
+      return -1;
+    return 0;
+  }
+
   public static void main(String[] args) throws Exception {
     if (args.length != 3) {
       throw new Error("You must provide: <month> <day> <year>");
@@ -51,8 +93,20 @@ public class Date {
     int d = Integer.parseInt(args[1]);
     int y = Integer.parseInt(args[2]);
 
-    Date birthday = new Date(m, d, y);
+    Date yourDate = new Date(m, d, y);
 
-    StdOut.println(birthday);
+    StdOut.println("Your Date is: " + yourDate);
+
+    StdOut.println("equals()");
+    Date date = new Date(9, 27, 1987);
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, date, yourDate.equals(date));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/27/1987", yourDate.equals(new Date(9, 27, 1987)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, null, yourDate.equals(null));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/27/1988", yourDate.equals(new Date(9, 27, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "10/27/1988", yourDate.equals(new Date(10, 27, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/26/1988", yourDate.equals(new Date(9, 26, 1988)));
+
+    StdOut.println("compareTo()");
+    StdOut.printf("%s <= %s ? %s\n", yourDate, date, yourDate.compareTo(date) <= 0);
   }
 }

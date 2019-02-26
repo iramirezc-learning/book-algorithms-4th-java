ea35bffeab240b9aaa9d0785aae25e0b6fd7f162
Isaac Ramirez <iramirezc@live.com.mx>
2019-02-26 00:19:31 -0600

    add equals method to Transaction ADT

--- chapter/1-fundamentals/section/2/data-type/Transaction.java
+++ chapter/1-fundamentals/section/2/data-type/Transaction.java
@@ -11,6 +11,14 @@ import edu.princeton.cs.algs4.StdOut;
  * $ java Transaction
  * Transaction1: Isaac 2/25/2019 99.99
  * Transaction2: Danni 5/13/2019 10000.0
+ * equals()
+ * Is 'Isaac 2/25/2019 500.0' equals to 'Isaac 2/25/2019 500.0'? true
+ * Is 'Isaac 2/25/2019 500.0' equals to 'Isaac 2/25/2019 500.0'? true
+ * Is 'Isaac 2/25/2019 500.0' equals to 'null'? false
+ * Is 'Isaac 2/25/2019 500.0' equals to '1/1/2019'? false
+ * Is 'Isaac 2/25/2019 500.0' equals to 'Isaac 3/25/2019 500.0'? false
+ * Is 'Isaac 2/25/2019 500.0' equals to 'Nahum 2/25/2019 500.0'? false
+ * Is 'Isaac 2/25/2019 500.0' equals to 'Isaac 2/25/2019 499.0'? false
  */
 
 /**
@@ -43,11 +51,48 @@ public class Transaction {
     return who() + " " + when() + " " + amount();
   }
 
+  public boolean equals(Object t) {
+    if (this == t)
+      return true;
+    if (t == null)
+      return false;
+    if (this.getClass() != t.getClass())
+      return false;
+
+    Transaction that = (Transaction) t;
+
+    if (!this.who().equals(that.who()))
+      return false;
+    if (!this.when().equals(that.when()))
+      return false;
+    if (this.amount() != that.amount())
+      return false;
+
+    return true;
+  }
+
   public static void main(String[] args) {
     Transaction transaction1 = new Transaction("Isaac", new Date(2, 25, 2019), 99.99);
     Transaction transaction2 = new Transaction("Danni", new Date(5, 13, 2019), 10000);
 
     StdOut.printf("Transaction1: %s\n", transaction1);
     StdOut.printf("Transaction2: %s\n", transaction2);
+
+    StdOut.println("equals()");
+
+    Transaction t1 = new Transaction("Isaac", new Date(2, 25, 2019), 500);
+    Transaction t2 = new Transaction("Isaac", new Date(2, 25, 2019), 500);
+    Transaction t3 = new Transaction("Isaac", new Date(3, 25, 2019), 500);
+    Transaction t4 = new Transaction("Nahum", new Date(2, 25, 2019), 500);
+    Transaction t5 = new Transaction("Isaac", new Date(2, 25, 2019), 499);
+
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, t1, t1.equals(t1)); // true
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, t2, t1.equals(t2)); // true
+
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, null, t1.equals(null)); // false
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, "1/1/2019", t1.equals(new Date(1, 1, 2019))); // false
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, t3, t1.equals(t3)); // false
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, t4, t1.equals(t4)); // false
+    StdOut.printf("Is '%s' equals to '%s'? %b\n", t1, t5, t1.equals(t5)); // false
   }
 }

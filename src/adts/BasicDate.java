f632d08fc98fc0a438f2899984ced682c3afab7e
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-22 23:16:29 -0500

    refactor BasicDate

--- src/adts/BasicDate.java
+++ src/adts/BasicDate.java
@@ -1,16 +1,19 @@
 import edu.princeton.cs.algs4.StdOut;
 
 /**
+ * BasicDate
  * Type: ADT
  * Description: An abstract data type to encapsulate dates.
- * Ref: p. 91, p. 247
+ * Chapter: 1
+ * Section: 2
+ * Ref: p. 91, p.103, p. 247
+ * Compilation: javac BasicDate.java
+ * Execution: java BasicDate <month> <day> <year>
+ * Example(s):
  *
- * Compilation: javac Date.java
- * Execution: java Date <month> <day> <year>
- *
- * $ java Date 9 27 1987
- * Your Date is: 9/27/1987
- * Parsed: 9/27/1987
+ * $ java BasicDate 9 27 1987
+ * new BasicDate(9, 27, 1987): 9/27/1987
+ * new BasicDate(9/27/1987): 9/27/1987
  *
  * equals()
  * Is 9/27/1987 equals to 9/27/1987? true
@@ -19,26 +22,24 @@ import edu.princeton.cs.algs4.StdOut;
  * Is 9/27/1987 equals to 9/27/1988? false
  * Is 9/27/1987 equals to 10/27/1988? false
  * Is 9/27/1987 equals to 9/26/1988? false
+ *
  * compareTo()
  * 9/27/1987 <= 9/27/1987 ? true
  */
-
-/**
- * Date
- */
-public class Date implements Comparable<Date> {
+public class BasicDate implements Comparable<BasicDate> {
   private final int month;
   private final int day;
   private final int year;
 
-  public Date(int m, int d, int y) {
+  public BasicDate(int m, int d, int y) {
     month = m;
     day = d;
     year = y;
   }
 
-  public Date(String date) {
+  public BasicDate(String date) {
     String[] fields = date.split("/");
+
     month = Integer.parseInt(fields[0]);
     day = Integer.parseInt(fields[1]);
     year = Integer.parseInt(fields[2]);
@@ -61,35 +62,48 @@ public class Date implements Comparable<Date> {
   }
 
   public boolean equals(Object x) {
-    if (this == x)
+    if (this == x) // if same reference
       return true;
-    if (x == null)
+
+    if (x == null) // if target is null
       return false;
-    if (this.getClass() != x.getClass())
+
+    if (this.getClass() != x.getClass()) // if classes are different
       return false;
-    Date that = (Date) x;
+
+    BasicDate that = (BasicDate) x;
+
     if (this.year() != that.year())
       return false;
+
     if (this.month() != that.month())
       return false;
+
     if (this.day() != that.day())
       return false;
+
     return true;
   }
 
-  public int compareTo(Date that) {
+  public int compareTo(BasicDate that) {
     if (this.year > that.year)
       return +1;
+
     if (this.year < that.year)
       return -1;
+
     if (this.month > that.month)
       return +1;
+
     if (this.month < that.month)
       return -1;
+
     if (this.day > that.day)
       return +1;
+
     if (this.day < that.day)
       return -1;
+
     return 0;
   }
 
@@ -102,22 +116,22 @@ public class Date implements Comparable<Date> {
     int d = Integer.parseInt(args[1]);
     int y = Integer.parseInt(args[2]);
 
-    Date yourDate = new Date(m, d, y);
+    BasicDate yourDate = new BasicDate(m, d, y);
 
-    StdOut.println("Your Date is: " + yourDate);
+    StdOut.println("new BasicDate(" + m + ", " + d + ", " + y + "): " + yourDate);
+    StdOut.println("new BasicDate(" + yourDate.toString() + "): " + new BasicDate(yourDate.toString()));
 
-    StdOut.println("Parsed: " + new Date(yourDate.toString()) + "\n");
+    BasicDate date = new BasicDate(9, 27, 1987);
 
-    StdOut.println("equals()");
-    Date date = new Date(9, 27, 1987);
+    StdOut.println("\nequals()");
     StdOut.printf("Is %s equals to %s? %b\n", yourDate, date, yourDate.equals(date));
-    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/27/1987", yourDate.equals(new Date(9, 27, 1987)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/27/1987", yourDate.equals(new BasicDate(9, 27, 1987)));
     StdOut.printf("Is %s equals to %s? %b\n", yourDate, null, yourDate.equals(null));
-    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/27/1988", yourDate.equals(new Date(9, 27, 1988)));
-    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "10/27/1988", yourDate.equals(new Date(10, 27, 1988)));
-    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/26/1988", yourDate.equals(new Date(9, 26, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/27/1988", yourDate.equals(new BasicDate(9, 27, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "10/27/1988", yourDate.equals(new BasicDate(10, 27, 1988)));
+    StdOut.printf("Is %s equals to %s? %b\n", yourDate, "9/26/1988", yourDate.equals(new BasicDate(9, 26, 1988)));
 
-    StdOut.println("compareTo()");
+    StdOut.println("\ncompareTo()");
     StdOut.printf("%s <= %s ? %s\n", yourDate, date, yourDate.compareTo(date) <= 0);
   }
 }

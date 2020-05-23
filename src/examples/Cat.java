a5644b66b07d41c8016bb6ab7f74e4b15c311d7e
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-22 21:00:48 -0500

    refactor Cat example

--- src/examples/Cat.java
+++ src/examples/Cat.java
@@ -2,33 +2,35 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Out;
 
 /**
+ * Cat
  * Type: Snippet
  * Description: Concatenates multiple files into one.
+ * Chapter: 1
+ * Section: 2
  * Ref: p. 82
- * Compilation: javac CatFile.java
- * Execution: java CatFile <input_file_1> <input_file_2> <output_file>
+ * Compilation: javac Cat.java
+ * Execution: java Cat <input_file_1> <input_file_2> <output_file>
+ * Example(s):
  *
- * $ java CatFile ~/algs4-data/in1.txt ~/algs4-data/in2.txt ~/algs4-data/out/out.txt
- * $ more ../out/out.txt
+ * $ java Cat ~/algs4-data/in1.txt ~/algs4-data/in2.txt ../out/out.txt && more ../out/out.txt
  * This is
  * a tiny
  * test.
  */
-
-/**
- * CatFile
- */
-public class CatFile {
+public class Cat {
   public static void main(String[] args) {
     // copy input files to out (last argument)
     Out out = new Out(args[args.length - 1]);
+
     for (int i = 0; i < args.length - 1; i++) {
       // copy input file named on the ith arg to out.
       In in = new In(args[i]);
       String s = in.readAll();
+
       out.println(s);
       in.close();
     }
+
     out.close();
   }
 }

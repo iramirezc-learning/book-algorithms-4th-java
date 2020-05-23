3b29a25950b00a89c012d980b8d36595dd46b57b
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-22 22:48:50 -0500

    refactor StaticSETofInts and Whitelist clients

--- src/examples/Whitelist.java
+++ src/examples/Whitelist.java
@@ -3,21 +3,24 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
+ * Whitelist
  * Type: Snippet
- * Description: Whitelist. A client for StaticSETofInts that uses Binary Search
+ * Description: A client for StaticSETofInts that uses Binary Search
+ * Chapter: 1
+ * Section: 2
  * Ref: p. 99
- * 
  * Compilation: javac Whitelist.java
  * Execution: java Whitelist < <input_file>
- * 
- * $ java Whitelist ~/algs4-data/tinyW.txt < ~/algs4-data/tinyT.txt
- * 50
- * 99
- * 13
- */
-
-/**
- * Whitelist
+ * Example(s):
+ *
+ * $ java Whitelist ~/algs4-data/largeW.txt < ~/algs4-data/largeT.txt | more
+ * 499569
+ * 984875
+ * 295754
+ * 207807
+ * 140925
+ * 161828
+ * ... # truncated
  */
 public class Whitelist {
   public static void main(String[] args) throws Exception {
@@ -28,8 +31,10 @@ public class Whitelist {
     In in = new In(args[0]);
     int[] a = in.readAllInts();
     StaticSETofInts set = new StaticSETofInts(a);
+
     while (!StdIn.isEmpty()) {
       int key = StdIn.readInt();
+
       if (!set.contains(key)) {
         StdOut.println(key);
       }

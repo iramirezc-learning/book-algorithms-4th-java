454ad76a021e546c919332f498cc3baaa8f94912
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-22 20:40:24 -0500

    refactor Counter ADT

--- src/adts/Counter.java
+++ src/adts/Counter.java
@@ -2,20 +2,20 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
+ * Counter
  * Type: ADT
  * Description: An abstract data type for a simple counter.
- * Ref: p. 84
+ * Chapter: 1
+ * Section: 2
+ * Ref: p. 85, 89
  * Compilation: javac Counter.java
  * Execution: java Counter <trials>
+ * Example(s):
  *
  * $ java Counter 10
  * 7 heads
  * 3 tails
  * delta: 4
- */
-
-/**
- * Counter
  */
 public class Counter {
   private final String name;
@@ -42,7 +42,6 @@ public class Counter {
       throw new Error("You must provide: <n_trials>");
     }
 
-    // Ref: Flips p. 89
     int trials = Integer.parseInt(args[0]);
     Counter heads = new Counter("heads");
     Counter tails = new Counter("tails");
@@ -59,6 +58,7 @@ public class Counter {
     StdOut.println(tails);
 
     int delta = heads.tally() - tails.tally();
+
     StdOut.println("delta: " + Math.abs(delta));
   }
 }

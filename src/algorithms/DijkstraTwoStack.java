ae0cffbdf08d2ec73334421a1343a43aba497569
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-30 20:08:30 -0500

    refactor DijkstraTwoStack algorithm

--- src/algorithms/DijkstraTwoStack.java
+++ src/algorithms/DijkstraTwoStack.java
@@ -2,31 +2,28 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
+ * DijkstraTwoStack
  * Type: Algorithm
  * Description: Dijkstra's Two-Stack Algorithm for Expression Evaluation
+ * Section: 1.3
  * Ref: p. 129
+ * Compilation: javac DijkstraTwoStack.java
+ * Execution: java DijkstraTwoStack < <standard_input>
+ * Example(s):
  *
- * Compilation: javac Evaluate.java
- * Execution: java Evaluate < <standard_input>
- *
- * $ java Evaluate
+ * $ java DijkstraTwoStack
  * Reading from Standard Input...
- * $ ( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )
- * $ <ctrl + d>
+ * > ( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )
+ * > <ctrl + d>
  * Expression value: 101.000
  *
- * $ java Evaluate
+ * $ java DijkstraTwoStack
  * Reading from Standard Input...
- * $ ( ( 1 + sqrt ( 5.0 ) ) / 2.0 )
- * $ <ctrl + d>
+ * > ( ( 1 + sqrt ( 5.0 ) ) / 2.0 )
+ * > <ctrl + d>
  * Expression value: 1.618
  */
-
-/**
- * Evaluate
- */
-public class Evaluate {
-
+public class DijkstraTwoStack {
   public static void main(String[] args) {
     Stack<String> operators = new Stack<String>();
     Stack<Double> values = new Stack<Double>();

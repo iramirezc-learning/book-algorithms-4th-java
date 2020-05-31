91c7c2372f8a24e5fb3b5f66691562b49c463c76
Isaac Ramirez <iramirezc@live.com.mx>
2020-05-30 20:06:03 -0500

    refactor Reverse snippet

--- src/examples/Reverse.java
+++ src/examples/Reverse.java
@@ -2,32 +2,31 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
+ * Reverse
  * Type: Snippet
  * Description: Sample Stack client.
+ * Section: 1.3
  * Ref: p. 127
  * Compilation: javac Reverse.java
  * Execution: java Reverse < <standard_input>
+ * Example(s):
  *
  * $ java Reverse
  * Reading from Standard Input...
- * $ 1
- * $ 2
- * $ 3
- * $ 4
- * $ 5
- * $ <ctrl-d>
+ * > 1
+ * > 2
+ * > 3
+ * > 4
+ * > 5
+ * > <ctrl-d>
+ * (5 items in the Stack):
  * 5
  * 4
  * 3
  * 2
  * 1
  */
-
-/**
- * Reverse
- */
 public class Reverse {
-
   public static void main(String[] args) {
     Stack<Integer> stack = new Stack<Integer>();
 
